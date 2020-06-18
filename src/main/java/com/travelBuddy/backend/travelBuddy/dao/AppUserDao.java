29da6ce76d5a8a737131c261df276a94b2015300
jiehang <jiehangwu.cs@gmail.com>
2020-06-17 21:49:09 -0700

    add script.sql

--- src/main/java/com/travelBuddy/backend/travelBuddy/dao/AppUserDao.java
+++ src/main/java/com/travelBuddy/backend/travelBuddy/dao/AppUserDao.java
@@ -4,5 +4,6 @@ import com.travelBuddy.backend.travelBuddy.entity.AppUser;
 
 public interface AppUserDao {
     AppUser findAppUserById(int id);
+    
     AppUser findAppUserByEmailPassword(String email, String password);
 }
--- src/main/script/script.sql
+++ src/main/script/script.sql
@@ -1,3 +1,109 @@
+CREATE TABLE Postcode (
+    code char(10),
+    city char(20),
+    country char(50),
+    PRIMARY KEY (code)
+);
+
+create table Geolocation (
+    longitude NUMERIC(7, 4),
+    latitude NUMERIC(7, 4),
+    streetNumber char(50),
+    postcode char(10),
+    primary key (latitude, longitude),
+    foreign key (postcode) references  Postcode on delete no action
+);
+
+create table CovidCase(
+    id serial,
+    longitude NUMERIC(7, 4),
+    latitude NUMERIC(7, 4),
+    caseType char(10),
+    primary key (id)
+);
+
+create table Contributor(
+    id serial,
+    username varchar,
+    password varchar,
+    primary key (id)
+);
+
+create table PublicArea(
+    id serial,
+    longitude NUMERIC(7, 4),
+    latitude NUMERIC(7, 4),
+    name char(50),
+    primary key (id),
+    foreign key (latitude, longitude) references Geolocation (latitude, longitude) on delete no action
+);
+
+create table AppUser(
+    id serial,
+    username varchar,
+    password varchar,
+    displayName varchar,
+    gender char(10),
+    age serial,
+    email varchar,
+    acceptedRiskLevel serial,
+    notificationMethod char(10),
+    primary key (id),
+    unique(email)
+);
+
+create table MakeComment(
+    id serial,
+    ccontent varchar,
+    upvote serial,
+    downvote serial,
+    longitude NUMERIC(7, 4) not null,
+    latitude NUMERIC(7, 4) not null,
+    userId serial not null,
+    primary key (id),
+    foreign key (latitude, longitude) references Geolocation(latitude, longitude) on delete no action,
+    foreign key (userId) references AppUser on delete no action
+);
+
+create table Suggestion(
+    id serial,
+    content varchar,
+    timestamp char(50),
+    primary key (id)
+);
+
+create table GiveSuggestion(
+    userId serial,
+    suggestionId serial,
+    primary key (userId, suggestionId),
+    foreign key (userId) references AppUser on delete cascade,
+    foreign key (suggestionId) references Suggestion on delete no action
+);
+
+create table Producer(
+    name varchar,
+    phone char(20),
+    email varchar,
+    primary key (name)
+);
+
+create table SafetyEquipment(
+    id serial,
+    type char(20),
+    producerName char(50),
+    description varchar,
+    primary key (id),
+    foreign key (producerName) references Producer on delete no action
+);
+
+create table ProvideEquipment(
+    publicAreaId serial,
+    equipmentId serial,
+    primary key (publicAreaId, equipmentId),
+    foreign key (publicAreaId) references PublicArea on delete cascade,
+    foreign key (equipmentId) references SafetyEquipment on delete no action
+);
+
 insert into Postcode(code, city, country) values('V6R2H7', 'Vancouver', 'Canada');
 insert into Postcode(code, city, country) values('V6T1E2', 'Vancuuver', 'Canada');
 insert into Postcode(code, city, country) values('V6R4R8', 'Vancouver', 'Canada');
@@ -194,10 +300,4 @@ insert into ProvideEquipment(publicareaid, equipmentid) values(7, 6);
 insert into ProvideEquipment(publicareaid, equipmentid) values(8, 10);
 insert into ProvideEquipment(publicareaid, equipmentid) values(9, 9);
 insert into ProvideEquipment(publicareaid, equipmentid) values(10, 10);
-
-
-
-
-
-
 
